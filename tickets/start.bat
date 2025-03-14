c202da50e6286ee45d479d4170d669764c1d3445
chaima zbidi <zbedichaima@gmail.com>
2025-03-14 13:57:13 +0100

    database configuration

---
+++ tickets/start.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Set environment variables
+set DB_PASSWORD=0000
+
+REM Start the application
+mvn spring-boot:run 
---
+++ tickets/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Set environment variables
+export DB_PASSWORD=0000
+
+# Start the application
+mvn spring-boot:run 
