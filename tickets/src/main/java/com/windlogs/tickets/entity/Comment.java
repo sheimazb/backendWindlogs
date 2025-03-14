95f18f615a67d118b46e4809aae77ee80bd40eb1
chaima zbidi <zbedichaima@gmail.com>
2025-03-14 13:53:43 +0100

    create comment entity

---
+++ tickets/src/main/java/com/windlogs/tickets/entity/Comment.java
@@ -0,0 +1,28 @@
+package com.windlogs.tickets.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private Long authorId;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
