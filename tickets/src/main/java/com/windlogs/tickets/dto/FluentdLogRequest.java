f00e1e4ec859e39ff8b733740426627a23aa625e
chaima zbidi <zbedichaima@gmail.com>
2025-03-28 13:26:19 +0100

    Create FluentdLogRequest.java

---
+++ tickets/src/main/java/com/windlogs/tickets/dto/FluentdLogRequest.java
@@ -0,0 +1,22 @@
+package com.windlogs.tickets.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class FluentdLogRequest {
+    // Basic log fields
+    private String level;
+    private String message;
+    private String source;
+    private long timestamp;
+    
+    // System identification fields
+    private String pid;
+    private String thread;
+    private String class_name;
+    private String container_id;
+    private String container_name;
+    private String tag;
+}
