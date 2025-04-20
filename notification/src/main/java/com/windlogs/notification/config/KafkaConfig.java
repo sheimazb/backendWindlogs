b72173dc458e8d8518c31a1239e7ec43606c3de6
chaima zbidi <zbedichaima@gmail.com>
2025-04-20 15:28:58 +0200

    update kafka

--- notification/src/main/java/com/windlogs/notification/config/KafkaConfig.java
+++ notification/src/main/java/com/windlogs/notification/config/KafkaConfig.java
@@ -1,5 +1,6 @@
 package com.windlogs.notification.config;
 
+import com.windlogs.notification.kafka.NotificationEvent;
 import com.windlogs.notification.kafka.TicketsLogEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -37,60 +38,43 @@ public class KafkaConfig {
 
     @Bean
     public ConsumerFactory<String, TicketsLogEvent> logEventConsumerFactory() {
-        logger.info("Configuring Kafka consumer with bootstrap servers: {}", bootstrapServers);
-        
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        
-        // Configure ErrorHandlingDeserializer as main deserializers
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-        
-        // Set the delegate deserializers 
-        props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
-        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class.getName());
-        
-        // Configure the JSON deserializer
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");  // Trust all packages for debugging
-        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.windlogs.notification.kafka.TicketsLogEvent");
-        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
-        props.put("spring.json.value.default.type", "com.windlogs.notification.kafka.TicketsLogEvent");
-        
-        // Set auto-offset-reset to earliest to not miss messages
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        
-        // More detailed logging
-        logger.info("Kafka consumer configuration: {}", props);
-        
-        return new DefaultKafkaConsumerFactory<>(props);
+        return new DefaultKafkaConsumerFactory<>(getCommonConsumerConfig(), 
+            new ErrorHandlingDeserializer<>(new StringDeserializer()),
+            new ErrorHandlingDeserializer<>(new JsonDeserializer<>(TicketsLogEvent.class, false)));
+    }
+
+    @Bean
+    public ConsumerFactory<String, NotificationEvent> notificationEventConsumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(getCommonConsumerConfig(),
+            new ErrorHandlingDeserializer<>(new StringDeserializer()),
+            new ErrorHandlingDeserializer<>(new JsonDeserializer<>(NotificationEvent.class, false)));
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, TicketsLogEvent> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, TicketsLogEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        ConcurrentKafkaListenerContainerFactory<String, TicketsLogEvent> factory = 
+            new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(logEventConsumerFactory());
-        
-        // Configure error handler with proper backoff
-        DefaultErrorHandler errorHandler = new DefaultErrorHandler(
-            (record, exception) -> {
-                logger.error("Error processing Kafka message: {}", exception.getMessage(), exception);
-                logger.error("Record: topic={}, partition={}, offset={}", 
-                    record.topic(), record.partition(), record.offset());
-                
-                // Log the received data for debugging
-                if (record.value() != null) {
-                    try {
-                        logger.error("Record value: {}", record.value().toString());
-                    } catch (Exception e) {
-                        logger.error("Could not convert record value to string", e);
-                    }
-                }
-            },
-            new FixedBackOff(1000L, 3) // Retry 3 times with 1 second interval
-        );
-        
-        factory.setCommonErrorHandler(errorHandler);
+        factory.setCommonErrorHandler(new DefaultErrorHandler(new FixedBackOff(1000L, 2L)));
         return factory;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, NotificationEvent> notificationKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, NotificationEvent> factory = 
+            new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(notificationEventConsumerFactory());
+        factory.setCommonErrorHandler(new DefaultErrorHandler(new FixedBackOff(1000L, 2L)));
+        return factory;
+    }
+
+    private Map<String, Object> getCommonConsumerConfig() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+        return props;
     }
 } 
